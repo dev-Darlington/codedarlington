4cae55891d5ac21da8736c888a056de537e34de4
Darlington <darlingtonuchenna53@gmail.com>
2025-12-21 03:04:36 +0100

    major touch on Projects.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { Briefcase, Code, User } from "lucide-react"
 
 const About = () => {
   return (
-    <section className="py-25 px-4 relative">
+    <section id="about" className="py-25 px-4 relative">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                 About <span className="text-primary">Me</span>
@@ -10,8 +10,8 @@ const About = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                 <div className="space-y-6">
                     <h3 className="text-2xl font-semibold">Passionate Web Developer</h3>
-                    <p className="text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum adipisci impedit cum magni praesentium explicabo expedita reiciendis! Iste, adipisci deserunt.</p>
-                    <p className="text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum adipisci impedit cum magni praesentium explicabo expedita reiciendis! Iste, adipisci deserunt.</p>
+                    <p className="text-muted-foreground">A creative front-end developer crafting vibrant, interactive digital landscapes. I transform bold visions into seamless user journeys through innovative coding and artistic web design</p>
+                    <p className="text-muted-foreground">Drawing on 3 years of hands-on practice, I combine React and CSS to deliver high-performance applications. I prioritize accessibility and cross-browser compatibility in every digital project.</p>
                     <div className="flex flex-col md:flex-row gap-4 justify-center">
                         <button className="cosmic-btn">Get In Touch</button>
                         <button className="px-6 py-2 text-primary font-medium border border-primary rounded-full hover:border-primary/80 hover:scale-105 active:scale-95 transition-all duration-300">Download CV</button>
@@ -25,7 +25,7 @@ const About = () => {
                             </div>
                             <div className="text-left">
                                 <h3 className="font-semibold text-lg">Web Development</h3>
-                                <p className="text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, sit.</p>
+                                <p className="text-muted-foreground">Building digital experiences through creative coding, structural logic, and design.</p>
                             </div>
                         </div>
                     </div>
@@ -36,7 +36,7 @@ const About = () => {
                             </div>
                             <div className="text-left">
                                 <h3 className="font-semibold text-lg">UI/UX Design</h3>
-                                <p className="text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, sit.</p>
+                                <p className="text-muted-foreground">Crafting intuitive interfaces through user research, wireframing, and visual harmony.</p>
                             </div>
                         </div>
                     </div>
@@ -47,7 +47,7 @@ const About = () => {
                             </div>
                             <div className="text-left">
                                 <h3 className="font-semibold text-lg">Technical Writing</h3>
-                                <p className="text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, sit.</p>
+                                <p className="text-muted-foreground">Simplifying complex information into clear, accessible, and highly accurate documentation.</p>
                             </div>
                         </div>
                     </div>
